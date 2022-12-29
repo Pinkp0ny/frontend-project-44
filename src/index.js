253b8d4a9f882f3c83724918700a7448201d6533
Pinkp0ny <a.kurganovv@gmail.com>
2022-12-29 21:39:13 +0300

    fix 3

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ console.log(task);
             console.log('Correct!');
         }
         else{
-            console.log(userAnswer, "is wrong answer ;(. Correct answer was " + correctAnswer +". \n Let's try again,", username, "!");
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${username}!`);
             return;
         }
     }
